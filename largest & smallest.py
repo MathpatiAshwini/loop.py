b7ace62a249b7d1c735dfffb63d562d1e1216a52
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2022-11-04 23:38:07 +0530

    Add files via upload

---
+++ largest & smallest.py
@@ -0,0 +1,10 @@
+# a=int(input("enter the nmber:"))
+# n=int(input("enter the number:"))
+# i=0
+# while i<a:
+#     if a>n and n<a:
+#         print(a,"is largest & ",n," is smallest")
+#     else:
+#         print(a,"is smallest & ",n,"is largest")
+#     i=n-i
+
---
+++ leap year..py
@@ -0,0 +1,12 @@
+
+# a=int(input("enter the year:-"))
+# i=1
+# while i<=3:
+#     if a%4==0:
+#         print(a,end=" ")
+#         a=a-4
+#     else:
+#         print(a,end=" ")
+# #         a=a+4
+#     i=i+1
+#     print()
