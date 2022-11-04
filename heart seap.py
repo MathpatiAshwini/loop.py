59451fb965c4f7c9e3e50e6f1558674e381d32cc
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2022-11-04 23:36:03 +0530

    Add files via upload

---
+++ heart seap.py
@@ -0,0 +1,24 @@
+# col=7
+# i=1
+# while i<=row: 
+#     j=1
+#     while j<=col:
+#         if (row==0 and col%3!=0) or (row==1 and col%3==0) or (row-col==2) or (row+col==8):
+#             print("*",end=" ")
+#             j=j+1
+#         else:
+#             print(" ",end=" ")
+#         print()
+#         row=6
+# i=i+1
+
+
+
+
+# i=1
+# while True:
+#     if i%2==0:
+#         break
+#     print(i)
+#     i=i+2
+
