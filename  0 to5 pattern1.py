12795b6fbf8a20d1fea60b8cf9f6c069e8d28204
Ashwini Mathpati <110845470+MathpatiAshwini@users.noreply.github.com>
2022-11-04 23:19:27 +0530

    Add files via upload

---
+++  0 to5 pattern1.py
@@ -0,0 +1,20 @@
+# i=1
+# while i<=5:
+#     j=0
+#     while j<i:
+#         print(j,end="")
+#         j=j+1
+#     i=i+1
+#     print()
+
+# (both output will be same)
+
+# i=0
+# while i<=5:
+#     j=0
+#     while i>j:
+#         print(j,end="")
+#         j=j+
+#     i=i+1
+#     print()
+
